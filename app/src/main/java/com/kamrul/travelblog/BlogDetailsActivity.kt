02961f3985aa221c2b3f4736950a95baf5e8de60
Kamrul Hasan <mkhrussell@gmail.com>
2022-03-26 16:23:02 -0400

    Image Loading

--- app/src/main/java/com/kamrul/travelblog/BlogDetailsActivity.kt
+++ app/src/main/java/com/kamrul/travelblog/BlogDetailsActivity.kt
@@ -2,7 +2,16 @@ package com.kamrul.travelblog;
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.bitmap.DrawableTransformation
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.kamrul.travelblog.databinding.ActivityBlogDetailsBinding
+
+private const val IMAGE_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+        "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg"
+private const val AVATAR_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+        "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg"
 
 class BlogDetailsActivity : AppCompatActivity() {
     lateinit var binding: ActivityBlogDetailsBinding
@@ -13,8 +22,16 @@ class BlogDetailsActivity : AppCompatActivity() {
         binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageMain.setImageResource(R.drawable.sydney_image)
-        binding.imageAvatar.setImageResource(R.drawable.avatar)
+        Glide.with(this)
+            .load(IMAGE_URL)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageMain)
+
+        Glide.with(this)
+            .load(AVATAR_URL)
+            .transform(CircleCrop())
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageAvatar)
 
         binding.textTitle.text = "G'day from Sydney"
         binding.textDate.text = "August 2, 2019"
