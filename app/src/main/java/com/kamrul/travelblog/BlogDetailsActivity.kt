591443add7ac65f2c56d37fc64bdd35a0911f4f7
Kamrul Hasan <mkhrussell@gmail.com>
2022-03-26 16:54:12 -0400

    Load data from the internet

--- app/src/main/java/com/kamrul/travelblog/BlogDetailsActivity.kt
+++ app/src/main/java/com/kamrul/travelblog/BlogDetailsActivity.kt
@@ -1,17 +1,14 @@
 package com.kamrul.travelblog;
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.bumptech.glide.load.resource.bitmap.DrawableTransformation
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.kamrul.travelblog.databinding.ActivityBlogDetailsBinding
-
-private const val IMAGE_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg"
-private const val AVATAR_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg"
+import com.kamrul.travelblog.http.Blog
+import com.kamrul.travelblog.http.BlogHttpClient
 
 class BlogDetailsActivity : AppCompatActivity() {
     lateinit var binding: ActivityBlogDetailsBinding
@@ -22,26 +19,43 @@ class BlogDetailsActivity : AppCompatActivity() {
         binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Glide.with(this)
-            .load(IMAGE_URL)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(binding.imageMain)
+        binding.imageBack.setOnClickListener { finish() }
+
+        loadData()
+    }
+
+    private fun loadData() {
+        BlogHttpClient.loadBlogArticles(
+            onSuccess = { list: List<Blog> ->
+                runOnUiThread {
+                    showData(list[0])
+                }
+            },
+            onError = {
+
+            }
+        )
+    }
+
+    private fun showData(blog: Blog) {
+        binding.progressBar.visibility = View.GONE
+        binding.textTitle.text = blog.title
+        binding.textDate.text = blog.date
+        binding.textAuthor.text = blog.author.name
+        binding.textRating.text = blog.rating.toString()
+        binding.textViews.text = String.format("(%d views)", blog.views)
+        binding.textDescription.text = blog.description
+        binding.ratingBar.rating = blog.rating
+        binding.ratingBar.visibility = View.VISIBLE
 
         Glide.with(this)
-            .load(AVATAR_URL)
+            .load(blog.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageMain)
+        Glide.with(this)
+            .load(blog.author.avatar)
             .transform(CircleCrop())
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(binding.imageAvatar)
-
-        binding.textTitle.text = "G'day from Sydney"
-        binding.textDate.text = "August 2, 2019"
-        binding.textAuthor.text = "Grayson Wells"
-        binding.textRating.text = "4.4"
-        binding.textViews.text = "(2687 views)"
-        binding.textDescription.text = "Australia is one of the most popular travel destinations in the world."
-
-        binding.ratingBar.rating = 4.4f
-
-        binding.imageBack.setOnClickListener { finish() }
     }
 }
